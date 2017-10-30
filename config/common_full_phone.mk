34e1dcbff9f87d9daf983af7044c625f936c0fca
ezio84 <brabus84@gmail.com>
2017-10-30 12:04:58 +0100

    Switch to Pixel2XL 8.1 sounds
    Change-Id: Ic571cd228dbfcad1992d0071cc4b649f845fdc05

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,9 +1,6 @@
 # Inherit common caosp stuff
 $(call inherit-product, vendor/caosp/config/common.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.alarm_alert=Krypton.ogg
-
 # Required caosp packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
