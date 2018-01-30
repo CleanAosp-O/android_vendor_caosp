camcory/vendor_androidx

AOSP Keyboard: Fix swipe FC

config/common.mk
@@ -28,6 +28,10 @@ PRODUCT_COPY_FILES +=  \
     vendor/androidx/prebuilt/common/su:root/sbin/su \
     vendor/androidx/prebuilt/common/app/Superuser.apk:system/app/Superuser/Superuser.apk
 
+# Latin IME lib
+PRODUCT_COPY_FILES += \
+    vendor/androidx/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

CleanAosp-O/android_vendor_caosp

config/common.mk
@@ -23,6 +23,10 @@ PRODUCT_COPY_FILES +=  \
     vendor/caosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/caosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
+# Latin IME lib
+PRODUCT_COPY_FILES += \
+    vendor/caosp/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml